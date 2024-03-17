524381d89f80aa8d7cd007ea92245b7d8262440e
Babul Raj <babul.raj@moengagae.com>
2024-03-17 11:59:22 +0530

    changed dependancies to range

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["MoEngageTriggerEvaluatorSPM","MoEngageTriggerEvaluator"]),
     ],
     dependencies: [
-        .package(name: "MoEngage-iOS-SDK",url: "https://github.com/moengage/MoEngage-iOS-SDK.git", from: "9.16.0"),
+        .package(url: "https://github.com/moengage/MoEngage-iOS-SDK.git", "9.16.0"..<"9.17.0"),
     ],
     targets: [
         .target(name: "MoEngageTriggerEvaluatorSPM", dependencies: ["MoEngage-iOS-SDK"], path: "Sources/..",exclude: ["MoEngageTriggerEvaluator.podspec", "README.md","./Frameworks/MoEngageTriggerEvaluator.xcframework", "LICENSE","CHANGELOG.md","Images/moe_logo_blue.png"]),
